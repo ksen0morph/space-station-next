LaCumbiaDelCoronavirus/KS16

Added Mindcontrol Implant (#513)
* init

* more typos

* Revert "more typos"

This reverts commit b101d383c197e89822852249f7ea0a19517c7007.

* audio and status icon

* darn typos

* init

* some fixes. and now prevents self injection

* moved the new component

* typo

* typo

* missed a check in shutdown

* changed all mindcontroll to mindcontrol

* removed unused

* by request

* samm fix to localisations

* nitpick , i like nitpick

---------

Co-authored-by: Fishbait <Fishbait@git.ml>

Content.Server/Goobstation/Roles/MindcontrolledRoleComponent.cs
@@ -1,0 +1,9 @@
+using Content.Shared.Roles;
+
+namespace Content.Server.Roles;
+
+[RegisterComponent]
+public sealed partial class MindcontrolledRoleComponent : AntagonistRoleComponent
+{
+    [DataField] public EntityUid? MasterUid = null;
+}

ksen0morph/space-station-next

Content.Server/Goobstation/Roles/MindcontrolledRoleComponent.cs
@@ -1,0 +1,9 @@
+using Content.Shared.Roles;
+
+namespace Content.Server.Roles;
+
+[RegisterComponent]
+public sealed partial class MindcontrolledRoleComponent : BaseMindRoleComponent
+{
+    [DataField] public EntityUid? MasterUid = null;
+}