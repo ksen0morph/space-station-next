a3154adab17462451048492030279094acaefa43
ksen0morph <144564556+ksen0morph@users.noreply.github.com>
2025-03-07 17:16:09 +0300

    Revert "Night and thermal vision device (#104)"
    This reverts commit 2ee7c2999cadcfea758eed9974f53380f31a29ee.

--- Content.Server/_CorvaxNext/Overlays/NightVisionSystem.cs
+++
@@ -1,5 +0,0 @@
-using Content.Shared._CorvaxNext.Overlays;
-
-namespace Content.Server._CorvaxNext.Overlays;
-
-public sealed class NightVisionSystem : SwitchableOverlaySystem<NightVisionComponent, ToggleNightVisionEvent>;
--- Content.Server/_CorvaxNext/Overlays/ThermalVisionSystem.cs
+++
@@ -1,5 +0,0 @@
-using Content.Shared._CorvaxNext.Overlays;
-
-namespace Content.Server._CorvaxNext.Overlays;
-
-public sealed class ThermalVisionSystem : SwitchableOverlaySystem<ThermalVisionComponent, ToggleThermalVisionEvent>;
